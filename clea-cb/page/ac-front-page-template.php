1e0b477355ef4293069ca5b63e304d5b38767f8d
aldelpech <al.delpech@parcours-performance.com>
2015-11-04 19:05:20 +0100

    MAJ loop page d'accueil

--- clea-cb/page/ac-front-page-template.php
+++ clea-cb/page/ac-front-page-template.php
@@ -11,68 +11,63 @@ get_header(); // Loads the header.php template. ?>
 
 	<section class="cb-post-list">
 
-		<!-- Begin excerpts area. -->
-		<?php 
-		// set the "paged" parameter (use 'page' if the query is on a static front page)
+<?php 
+
+		//Protect against arbitrary paged values
+		// http://codex.wordpress.org/Function_Reference/paginate_links
 		$paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
-		
+
 		$loop = new WP_Query(
 			array(
-				'post_type' => 'post',
-				'posts_per_page' => 8,
+				'post_type' 		=> 'post',
+				'posts_per_page' 	=> 8,
+				'paged'				=> $paged,
+				'ignore_sticky_posts' => true, /* will not show featured posts first */
+				'orderby' => 'date', /* 'modified' last modified 'date' would order by published date */
+				'order' => 'DESC',         /* 'ASC' */
 				'tax_query' => array(
 					array(
 						'taxonomy' => 'post_format',
-						'field' => 'slug',
-						'terms' => array(
-							'post-format-aside',
-							'post-format-audio',
-							'post-format-chat',
-							'post-format-gallery',
-							'post-format-image', 
-							'post-format-link', 
-							'post-format-quote',
-							'post-format-status',
-							'post-format-video'
-						),
-						'operator' => 'NOT IN'
+						'field' 	=> 'slug',
+						'terms' 	=> array(
+										'post-format-aside',
+										'post-format-audio',
+										'post-format-chat',
+										'post-format-gallery',
+										'post-format-image', 
+										'post-format-link', 
+										'post-format-quote',
+										'post-format-status',
+										'post-format-video'
+									),
+						'operator' 	=> 'NOT IN'
 					)
 				),
 				'category__not_in'	=> '72', /* exclude category "AIDE" */
-				'post__not_in' => $do_not_duplicate,
-				'paged' 			=> $paged,
-				'post_status' => 'publish', /* show only published posts */
+				'post__not_in' => $do_not_duplicate
 			)
 		); ?>
 
-		<?php if ( $loop->have_posts() ) : ?>
+		
+		<h2>Les derniers articles du blog</h2>
+		
+		<?php get_template_part( 'loop-meta' ); // Loads the loop-meta.php template. ?>		
+		
+		<?php if ( $loop->have_posts() ) { ?>
+
+			<?php while ( $loop->have_posts() ) : $loop->the_post(); $do_not_duplicate[] = get_the_ID(); ?>
+
+				<?php get_template_part( 'content', ( post_type_supports( get_post_type(), 'post-formats' ) ? get_post_format() : get_post_type() ) ); ?>
 
 
-				<?php while ( $loop->have_posts() ) : $loop->the_post(); $do_not_duplicate[] = get_the_ID();  ?>
+			<?php endwhile; ?>
+
+		<?php } ?>
+
+		<a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">Tous les articles du Blog</a>
+
 	
-					<?php get_template_part( 'content', ( post_type_supports( get_post_type(), 'post-formats' ) ? get_post_format() : get_post_type() ) ); ?>
-
-				<?php endwhile; ?>
-
-
-		<?php endif; ?>
-		<!-- End excerpts area. --->
-
-
-		<!-- http://wordpress.stackexchange.com/questions/174907/how-to-use-the-posts-navigation-for-wp-query-and-get-posts -->
-		<?php 
-		$GLOBALS['wp_query']->max_num_pages = $loop->max_num_pages;
-		
-		// echos the return of get_the_posts_pagination()
-		the_posts_pagination( array(
-			'mid_size' => 1,
-			'prev_text' => __( 'Plus récents', 'clea-base' ),
-			'next_text' => __( 'Plus anciens', 'clea-base' ),
-			'screen_reader_text' => __( 'Autres articles', 'clea-base' ),
-		) ); ?>
-		
-		
-		<?php wp_reset_query(); ?>
+<?php wp_reset_query(); ?>
 
 	</section><!-- #section -->	
 	
